c0ff74aed4dd1c701084a188c8f46ba037affe8c
becker oliver utia lopez <becker_utia_123@hotmail.com>
2019-10-10 22:56:04 -0500

    publishing a message

--- RabbitMQConsoleTest/RabbitMQConsoleTest/Program.cs
+++ RabbitMQConsoleTest/RabbitMQConsoleTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using RabbitMQ.Client;
 using System;
+using System.Text;
 
 namespace RabbitMQConsoleTest
 {
@@ -9,21 +10,40 @@ namespace RabbitMQConsoleTest
         private const string UserName = "guest";
         private const string Password = "guest";
 
+        private const string QueueName = "MyQueue";
+        private const string ExchangeName = "";
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting RabbitMQ Message Sender");
+            Console.WriteLine();
+            Console.WriteLine();
+
             var connectionFactory = new RabbitMQ.Client.ConnectionFactory() { HostName = HostName, UserName = UserName, Password = Password };
             var connection = connectionFactory.CreateConnection();
             var model = connection.CreateModel();
             //model.BasicPublish();
 
-            model.QueueDeclare("MyQueue", true, false, false, null);
-            Console.WriteLine("Queue creates");
+            //model.QueueDeclare("MyQueue", true, false, false, null);
+            //Console.WriteLine("Queue creates");
 
-            model.ExchangeDeclare("MyExchange", ExchangeType.Topic);
-            Console.WriteLine("Exchange created");
+            //model.ExchangeDeclare("MyExchange", ExchangeType.Topic);
+            //Console.WriteLine("Exchange created");
 
-            model.QueueBind("MyQueue", "MyExchange", "cars");
-            Console.WriteLine("Hello World!");
+            //model.QueueBind("MyQueue", "MyExchange", "cars");
+            //Console.WriteLine("Hello World!");
+
+            var properties = model.CreateBasicProperties();
+            properties.Persistent = false;
+
+            //Serialize
+            byte[] messageBuffer = Encoding.Default.GetBytes("This is my message");
+
+
+            //Send Message
+            model.BasicPublish(ExchangeName, QueueName, properties, messageBuffer);
+
+            Console.WriteLine("Message sent");
+            Console.ReadLine();
         }
     }
 }
