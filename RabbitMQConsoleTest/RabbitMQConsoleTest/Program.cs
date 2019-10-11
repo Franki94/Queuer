f5954f397ba6ff27ac6f3435a17d25d7597528e0
becker oliver utia lopez <becker_utia_123@hotmail.com>
2019-10-10 22:03:36 -0500

    creatin connection rabbitMQ

---
+++ RabbitMQConsoleTest/RabbitMQConsoleTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RabbitMQConsoleTest
+{
+    class Program
+    {
+        private const string HostName = "localhost";
+        private const string UserName = "guest";
+        private const string Password = "guest";
+
+        static void Main(string[] args)
+        {
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory() { HostName = HostName, UserName = UserName, Password = Password };
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+            //model.BasicPublish();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
